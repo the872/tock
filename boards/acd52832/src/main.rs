9597c226792b806e78365e829535a6d03393f2c8
Brad Campbell <bradjc5@gmail.com>
2018-06-18 18:50:49 -0400

    add i2c to main.rs

--- boards/acd52832/src/main.rs
+++ boards/acd52832/src/main.rs
@@ -16,9 +16,9 @@ use capsules::virtual_alarm::VirtualMuxAlarm;
 use nrf5x::rtc::Rtc;
 
 const LED1_PIN: usize = 26;
-const LED2_PIN: usize = 18;
-const LED3_PIN: usize = 19;
-const LED4_PIN: usize = 20;
+const LED2_PIN: usize = 22;
+const LED3_PIN: usize = 23;
+const LED4_PIN: usize = 24;
 
 const BUTTON1_PIN: usize = 25;
 const BUTTON2_PIN: usize = 14;
@@ -51,7 +51,7 @@ pub struct Platform {
         VirtualMuxAlarm<'static, Rtc>,
     >,
     button: &'static capsules::button::Button<'static, nrf5x::gpio::GPIOPin>,
-    console: &'static capsules::console::Console<'static, nrf52::uart::Uarte>,
+    // console: &'static capsules::console::Console<'static, nrf52::uart::Uarte>,
     gpio: &'static capsules::gpio::GPIO<'static, nrf5x::gpio::GPIOPin>,
     led: &'static capsules::led::LED<'static, nrf5x::gpio::GPIOPin>,
     rng: &'static capsules::rng::SimpleRng<'static, nrf5x::trng::Trng<'static>>,
@@ -69,7 +69,7 @@ impl kernel::Platform for Platform {
         F: FnOnce(Option<&kernel::Driver>) -> R,
     {
         match driver_num {
-            capsules::console::DRIVER_NUM => f(Some(self.console)),
+            // capsules::console::DRIVER_NUM => f(Some(self.console)),
             capsules::gpio::DRIVER_NUM => f(Some(self.gpio)),
             capsules::alarm::DRIVER_NUM => f(Some(self.alarm)),
             capsules::led::DRIVER_NUM => f(Some(self.led)),
@@ -226,28 +226,63 @@ pub unsafe fn reset_handler() {
         capsules::virtual_alarm::VirtualMuxAlarm::new(mux_alarm)
     );
 
-    nrf52::uart::UARTE0.configure(
-        nrf5x::pinmux::Pinmux::new(6), // tx
-        nrf5x::pinmux::Pinmux::new(8), // rx
-        nrf5x::pinmux::Pinmux::new(7), // cts
-        nrf5x::pinmux::Pinmux::new(5),
-    ); // rts
-    let console = static_init!(
-        capsules::console::Console<nrf52::uart::Uarte>,
-        capsules::console::Console::new(
-            &nrf52::uart::UARTE0,
-            115200,
-            &mut capsules::console::WRITE_BUF,
-            &mut capsules::console::READ_BUF,
-            kernel::Grant::create()
-        )
-    );
-    kernel::hil::uart::UART::set_client(&nrf52::uart::UARTE0, console);
-    console.initialize();
+    // nrf52::uart::UARTE0.configure(
+    //     nrf5x::pinmux::Pinmux::new(6), // tx
+    //     nrf5x::pinmux::Pinmux::new(8), // rx
+    //     nrf5x::pinmux::Pinmux::new(7), // cts
+    //     nrf5x::pinmux::Pinmux::new(5),
+    // ); // rts
+    // let console = static_init!(
+    //     capsules::console::Console<nrf52::uart::Uarte>,
+    //     capsules::console::Console::new(
+    //         &nrf52::uart::UARTE0,
+    //         115200,
+    //         &mut capsules::console::WRITE_BUF,
+    //         &mut capsules::console::READ_BUF,
+    //         kernel::Grant::create()
+    //     )
+    // );
+    // kernel::hil::uart::UART::set_client(&nrf52::uart::UARTE0, console);
+    // console.initialize();
 
-    // Attach the kernel debug interface to this console
-    let kc = static_init!(capsules::console::App, capsules::console::App::default());
-    kernel::debug::assign_console_driver(Some(console), kc);
+    // // Attach the kernel debug interface to this console
+    // let kc = static_init!(capsules::console::App, capsules::console::App::default());
+    // kernel::debug::assign_console_driver(Some(console), kc);
+
+
+    let i2c_mux = static_init!(capsules::virtual_i2c::MuxI2C<'static>, capsules::virtual_i2c::MuxI2C::new(&nrf52::i2c::TWIM0));
+    nrf52::i2c::TWIM0.configure(nrf5x::pinmux::Pinmux::new(21), nrf5x::pinmux::Pinmux::new(20));
+    nrf52::i2c::TWIM0.set_client(i2c_mux);
+
+    // Configure the MCP23008. Device address 0x20.
+    let mcp23008_i2c = static_init!(
+        capsules::virtual_i2c::I2CDevice,
+        capsules::virtual_i2c::I2CDevice::new(i2c_mux, 0x20));
+    let mcp23008 = static_init!(
+        capsules::mcp23008::MCP23008<'static>,
+        capsules::mcp23008::MCP23008::new(mcp23008_i2c,
+                                          Some(&nrf5x::gpio::PORT[11]),
+                                          Some(&nrf5x::gpio::PORT[12]),
+                                          &mut capsules::mcp23008::BUFFER,
+                                          8, 2));
+    mcp23008_i2c.set_client(mcp23008);
+    nrf5x::gpio::PORT[11].set_client(mcp23008);
+    nrf5x::gpio::PORT[12].set_client(mcp23008);
+
+    // Create an array of the GPIO extenders so we can pass them to an
+    // administrative layer that provides a single interface to them all.
+    let async_gpio_ports = static_init!(
+        [&'static capsules::mcp23008::MCP23008; 1],
+        [mcp23008]);
+
+    // `gpio_async` is the object that manages all of the extenders.
+    let gpio_async = static_init!(
+        capsules::gpio_async::GPIOAsync<'static, capsules::mcp23008::MCP23008<'static>>,
+        capsules::gpio_async::GPIOAsync::new(async_gpio_ports));
+    // Setup the clients correctly.
+    for port in async_gpio_ports.iter() {
+        port.set_client(gpio_async);
+    }
 
     let ble_radio = static_init!(
         capsules::ble_advertising_driver::BLE<
@@ -302,7 +337,7 @@ pub unsafe fn reset_handler() {
     let platform = Platform {
         button: button,
         ble_radio: ble_radio,
-        console: console,
+        // console: console,
         led: led,
         gpio: gpio,
         rng: rng,
