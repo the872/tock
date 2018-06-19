eacd00a19a845b66a36c17021fe1bb7dccde5e9d
Brad Campbell <bradjc5@gmail.com>
2018-06-18 22:06:32 -0400

    fix io.rs

--- boards/acd52832/src/io.rs
+++ boards/acd52832/src/io.rs
@@ -1,34 +1,14 @@
 use core::fmt::{Arguments, Write};
 use kernel::debug;
 use kernel::hil::led;
-use kernel::hil::uart::{self, UART};
-use nrf52;
 use nrf5x;
 
-struct Writer {
-    initialized: bool,
-}
+struct Writer { }
 
-static mut WRITER: Writer = Writer { initialized: false };
+static mut WRITER: Writer = Writer {  };
 
 impl Write for Writer {
-    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
-        let uart = unsafe { &mut nrf52::uart::UARTE0 };
-        if !self.initialized {
-            self.initialized = true;
-            uart.init(uart::UARTParams {
-                baud_rate: 115200,
-                stop_bits: uart::StopBits::One,
-                parity: uart::Parity::None,
-                hw_flow_control: false,
-            });
-        }
-        for c in s.bytes() {
-            unsafe {
-                uart.send_byte(c);
-            }
-            while !uart.tx_ready() {}
-        }
+    fn write_str(&mut self, _: &str) -> ::core::fmt::Result {
         Ok(())
     }
 }
@@ -38,8 +18,7 @@ impl Write for Writer {
 #[lang = "panic_fmt"]
 /// Panic handler
 pub unsafe extern "C" fn panic_fmt(args: Arguments, file: &'static str, line: u32) -> ! {
-    // The nRF52 DK LEDs (see back of board)
-    const LED1_PIN: usize = 17;
+    const LED1_PIN: usize = 14;
     let led = &mut led::LedLow::new(&mut nrf5x::gpio::PORT[LED1_PIN]);
     let writer = &mut WRITER;
     debug::panic(led, writer, args, file, line)
