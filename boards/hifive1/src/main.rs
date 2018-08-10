3b81f24508af39b2ce797e48b88f126e1a6f65ea
Brad Campbell <bradjc5@gmail.com>
2018-08-10 00:12:47 -0400

    cleanup

--- boards/hifive1/src/main.rs
+++ boards/hifive1/src/main.rs
@@ -19,7 +19,6 @@ use capsules::virtual_spi::{MuxSpiMaster, VirtualSpiMasterDevice};
 use capsules::virtual_uart::{UartDevice, UartMux};
 use kernel::hil;
 use kernel::hil::spi::SpiMaster;
-use kernel::hil::Controller;
 use kernel::Platform;
 
 pub mod io;
@@ -56,22 +55,9 @@ struct HiFive1 {
     //     'static,
     //     VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>,
     // >,
-    // ambient_light: &'static capsules::ambient_light::AmbientLight<'static>,
-    // temp: &'static capsules::temperature::TemperatureSensor<'static>,
-    // ninedof: &'static capsules::ninedof::NineDof<'static>,
-    // humidity: &'static capsules::humidity::HumiditySensor<'static>,
-    // spi: &'static capsules::spi::Spi<'static, VirtualSpiMasterDevice<'static, sam4l::spi::SpiHw>>,
-    // nrf51822: &'static capsules::nrf51822_serialization::Nrf51822Serialization<
-    //     'static,
-    //     sam4l::usart::USART,
-    // >,
-    // adc: &'static capsules::adc::Adc<'static, sam4l::adc::Adc>,
     led: &'static capsules::led::LED<'static, e310x::gpio::GpioPin>,
     // button: &'static capsules::button::Button<'static, sam4l::gpio::GPIOPin>,
-    // rng: &'static capsules::rng::SimpleRng<'static, sam4l::trng::Trng<'static>>,
     // ipc: kernel::ipc::IPC,
-    // crc: &'static capsules::crc::Crc<'static, sam4l::crccu::Crccu<'static>>,
-    // dac: &'static capsules::dac::Dac<'static>,
 }
 
 /// Mapping of integer syscalls to objects that implement syscalls.
@@ -85,21 +71,8 @@ impl Platform for HiFive1 {
             capsules::gpio::DRIVER_NUM => f(Some(self.gpio)),
 
             // capsules::alarm::DRIVER_NUM => f(Some(self.alarm)),
-            // capsules::spi::DRIVER_NUM => f(Some(self.spi)),
-            // capsules::nrf51822_serialization::DRIVER_NUM => f(Some(self.nrf51822)),
-            // capsules::ambient_light::DRIVER_NUM => f(Some(self.ambient_light)),
-            // capsules::adc::DRIVER_NUM => f(Some(self.adc)),
             capsules::led::DRIVER_NUM => f(Some(self.led)),
             // capsules::button::DRIVER_NUM => f(Some(self.button)),
-            // capsules::humidity::DRIVER_NUM => f(Some(self.humidity)),
-            // capsules::temperature::DRIVER_NUM => f(Some(self.temp)),
-            // capsules::ninedof::DRIVER_NUM => f(Some(self.ninedof)),
-
-            // capsules::rng::DRIVER_NUM => f(Some(self.rng)),
-
-            // capsules::crc::DRIVER_NUM => f(Some(self.crc)),
-
-            // capsules::dac::DRIVER_NUM => f(Some(self.dac)),
 
             // kernel::ipc::DRIVER_NUM => f(Some(&self.ipc)),
             _ => f(None),
@@ -107,66 +80,10 @@ impl Platform for HiFive1 {
     }
 }
 
-// /// Helper function called during bring-up that configures multiplexed I/O.
-// unsafe fn set_pin_primary_functions() {
-//     use sam4l::gpio::PeripheralFunction::{A, B};
-//     use sam4l::gpio::{PA, PB};
-
-//     PA[04].configure(Some(A)); // A0 - ADC0
-//     PA[05].configure(Some(A)); // A1 - ADC1
-//     PA[06].configure(Some(A)); // DAC
-//     PA[07].configure(None); //... WKP - Wakeup
-//     PA[08].configure(Some(A)); // FTDI_RTS - USART0 RTS
-//     PA[09].configure(None); //... ACC_INT1 - FXOS8700CQ Interrupt 1
-//     PA[10].configure(None); //... unused
-//     PA[11].configure(Some(A)); // FTDI_OUT - USART0 RX FTDI->SAM4L
-//     PA[12].configure(Some(A)); // FTDI_IN - USART0 TX SAM4L->FTDI
-//     PA[13].configure(None); //... RED_LED
-//     PA[14].configure(None); //... BLUE_LED
-//     PA[15].configure(None); //... GREEN_LED
-//     PA[16].configure(None); //... BUTTON - User Button
-//     PA[17].configure(None); //... !NRF_RESET - Reset line for nRF51822
-//     PA[18].configure(None); //... ACC_INT2 - FXOS8700CQ Interrupt 2
-//     PA[19].configure(None); //... unused
-//     PA[20].configure(None); //... !LIGHT_INT - ISL29035 Light Sensor Interrupt
-//                             // SPI Mode
-//     PA[21].configure(Some(A)); // D3 - SPI MISO
-//     PA[22].configure(Some(A)); // D2 - SPI MOSI
-//     PA[23].configure(Some(A)); // D4 - SPI SCK
-//     PA[24].configure(Some(A)); // D5 - SPI CS0
-//                                // // I2C MODE
-//                                // PA[21].configure(None); // D3
-//                                // PA[22].configure(None); // D2
-//                                // PA[23].configure(Some(B)); // D4 - TWIMS0 SDA
-//                                // PA[24].configure(Some(B)); // D5 - TWIMS0 SCL
-//                                // UART Mode
-//     PA[25].configure(Some(B)); // RX - USART2 RXD
-//     PA[26].configure(Some(B)); // TX - USART2 TXD
-
-//     PB[00].configure(Some(A)); // SENSORS_SDA - TWIMS1 SDA
-//     PB[01].configure(Some(A)); // SENSORS_SCL - TWIMS1 SCL
-//     PB[02].configure(Some(A)); // A2 - ADC3
-//     PB[03].configure(Some(A)); // A3 - ADC4
-//     PB[04].configure(Some(A)); // A4 - ADC5
-//     PB[05].configure(Some(A)); // A5 - ADC6
-//     PB[06].configure(Some(A)); // NRF_CTS - USART3 RTS
-//     PB[07].configure(Some(A)); // NRF_RTS - USART3 CTS
-//     PB[08].configure(None); //... NRF_INT - Interrupt line nRF->SAM4L
-//     PB[09].configure(Some(A)); // NRF_OUT - USART3 RXD
-//     PB[10].configure(Some(A)); // NRF_IN - USART3 TXD
-//     PB[11].configure(None); //... D6
-//     PB[12].configure(None); //... D7
-//     PB[13].configure(None); //... unused
-//     PB[14].configure(None); //... D0
-//     PB[15].configure(None); //... D1
-// }
-
 /// Reset Handler.
 ///
-/// This symbol is loaded into vector table by the SAM4L chip crate.
-/// When the chip first powers on or later does a hard reset, after the core
-/// initializes all the hardware, the address of this function is loaded and
-/// execution begins here.
+/// This function is called from the arch crate after some very basic RISC-V
+/// setup.
 #[no_mangle]
 pub unsafe fn reset_handler() {
     // Basic setup of the platform.
@@ -188,25 +105,19 @@ pub unsafe fn reset_handler() {
     // // Source 32Khz and 1Khz clocks from RC23K (SAM4L Datasheet 11.6.8)
     // sam4l::bpm::set_ck32source(sam4l::bpm::CK32Source::RC32K);
 
-    // set_pin_primary_functions();
 
     let board_kernel = static_init!(kernel::Kernel, kernel::Kernel::new(&PROCESSES));
 
     // Configure kernel debug gpios as early as possible
     kernel::debug::assign_gpios(
         Some(&e310x::gpio::PORT[22]),
-        Some(&e310x::gpio::PORT[22]),
-        Some(&e310x::gpio::PORT[22]),
-        // Some(&sam4l::gpio::PA[15]),
-        // Some(&sam4l::gpio::PA[14]),
+        None,
+        None,
     );
 
     let mut chip = e310x::chip::E310x::new();
 
 
-
-    // // Initialize USART0 for Uart
-    // e310x::usart::USART0.set_mode(sam4l::usart::UsartMode::Uart);
 
     // Create a shared UART channel for the console and for kernel debug.
     let uart_mux = static_init!(
@@ -234,20 +145,6 @@ pub unsafe fn reset_handler() {
     // );
     // hil::uart::UART::set_client(console_uart, console);
 
-    // // Initialize USART3 for Uart
-    // sam4l::usart::USART3.set_mode(sam4l::usart::UsartMode::Uart);
-    // // Create the Nrf51822Serialization driver for passing BLE commands
-    // // over UART to the nRF51822 radio.
-    // let nrf_serialization = static_init!(
-    //     capsules::nrf51822_serialization::Nrf51822Serialization<sam4l::usart::USART>,
-    //     capsules::nrf51822_serialization::Nrf51822Serialization::new(
-    //         &sam4l::usart::USART3,
-    //         &sam4l::gpio::PA[17],
-    //         &mut capsules::nrf51822_serialization::WRITE_BUF,
-    //         &mut capsules::nrf51822_serialization::READ_BUF
-    //     )
-    // );
-    // hil::uart::UART::set_client(&sam4l::usart::USART3, nrf_serialization);
 
     // let ast = &sam4l::ast::AST;
 
@@ -257,93 +154,8 @@ pub unsafe fn reset_handler() {
     // );
     // ast.configure(mux_alarm);
 
-    // let sensors_i2c = static_init!(MuxI2C<'static>, MuxI2C::new(&sam4l::i2c::I2C1));
-    // sam4l::i2c::I2C1.set_master_client(sensors_i2c);
 
-    // // SI7021 Temperature / Humidity Sensor, address: 0x40
-    // let si7021_i2c = static_init!(
-    //     capsules::virtual_i2c::I2CDevice,
-    //     capsules::virtual_i2c::I2CDevice::new(sensors_i2c, 0x40)
-    // );
-    // let si7021_virtual_alarm = static_init!(
-    //     VirtualMuxAlarm<'static, sam4l::ast::Ast>,
-    //     VirtualMuxAlarm::new(mux_alarm)
-    // );
-    // let si7021 = static_init!(
-    //     capsules::si7021::SI7021<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
-    //     capsules::si7021::SI7021::new(
-    //         si7021_i2c,
-    //         si7021_virtual_alarm,
-    //         &mut capsules::si7021::BUFFER
-    //     )
-    // );
-    // si7021_i2c.set_client(si7021);
-    // si7021_virtual_alarm.set_client(si7021);
 
-    // let temp = static_init!(
-    //     capsules::temperature::TemperatureSensor<'static>,
-    //     capsules::temperature::TemperatureSensor::new(si7021, board_kernel.create_grant())
-    // );
-    // kernel::hil::sensors::TemperatureDriver::set_client(si7021, temp);
-
-    // let humidity = static_init!(
-    //     capsules::humidity::HumiditySensor<'static>,
-    //     capsules::humidity::HumiditySensor::new(si7021, board_kernel.create_grant())
-    // );
-    // kernel::hil::sensors::HumidityDriver::set_client(si7021, humidity);
-
-    // // Configure the ISL29035, device address 0x44
-    // let isl29035_i2c = static_init!(I2CDevice, I2CDevice::new(sensors_i2c, 0x44));
-    // let isl29035_virtual_alarm = static_init!(
-    //     VirtualMuxAlarm<'static, sam4l::ast::Ast>,
-    //     VirtualMuxAlarm::new(mux_alarm)
-    // );
-    // let isl29035 = static_init!(
-    //     capsules::isl29035::Isl29035<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
-    //     capsules::isl29035::Isl29035::new(
-    //         isl29035_i2c,
-    //         isl29035_virtual_alarm,
-    //         &mut capsules::isl29035::BUF
-    //     )
-    // );
-    // isl29035_i2c.set_client(isl29035);
-    // isl29035_virtual_alarm.set_client(isl29035);
-
-    // let ambient_light = static_init!(
-    //     capsules::ambient_light::AmbientLight<'static>,
-    //     capsules::ambient_light::AmbientLight::new(isl29035, board_kernel.create_grant())
-    // );
-    // hil::sensors::AmbientLight::set_client(isl29035, ambient_light);
-
-    // // Alarm
-    // let virtual_alarm1 = static_init!(
-    //     VirtualMuxAlarm<'static, sam4l::ast::Ast>,
-    //     VirtualMuxAlarm::new(mux_alarm)
-    // );
-    // let alarm = static_init!(
-    //     capsules::alarm::AlarmDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
-    //     capsules::alarm::AlarmDriver::new(virtual_alarm1, board_kernel.create_grant())
-    // );
-    // virtual_alarm1.set_client(alarm);
-
-    // // FXOS8700CQ accelerometer, device address 0x1e
-    // let fxos8700_i2c = static_init!(I2CDevice, I2CDevice::new(sensors_i2c, 0x1e));
-    // let fxos8700 = static_init!(
-    //     capsules::fxos8700cq::Fxos8700cq<'static>,
-    //     capsules::fxos8700cq::Fxos8700cq::new(
-    //         fxos8700_i2c,
-    //         &sam4l::gpio::PA[9],
-    //         &mut capsules::fxos8700cq::BUF
-    //     )
-    // );
-    // fxos8700_i2c.set_client(fxos8700);
-    // sam4l::gpio::PA[9].set_client(fxos8700);
-
-    // let ninedof = static_init!(
-    //     capsules::ninedof::NineDof<'static>,
-    //     capsules::ninedof::NineDof::new(fxos8700, board_kernel.create_grant())
-    // );
-    // hil::sensors::NineDof::set_client(fxos8700, ninedof);
 
     // // Initialize and enable SPI HAL
     // // Set up an SPI MUX, so there can be multiple clients
@@ -355,21 +167,6 @@ pub unsafe fn reset_handler() {
     // sam4l::spi::SPI.set_client(mux_spi);
     // sam4l::spi::SPI.init();
 
-    // // Create a virtualized client for SPI system call interface
-    // // CS line is CS0
-    // let syscall_spi_device = static_init!(
-    //     VirtualSpiMasterDevice<'static, sam4l::spi::SpiHw>,
-    //     VirtualSpiMasterDevice::new(mux_spi, 0)
-    // );
-
-    // // Create the SPI system call capsule, passing the client
-    // let spi_syscalls = static_init!(
-    //     capsules::spi::Spi<'static, VirtualSpiMasterDevice<'static, sam4l::spi::SpiHw>>,
-    //     capsules::spi::Spi::new(syscall_spi_device)
-    // );
-
-    // spi_syscalls.config_buffers(&mut SPI_READ_BUF, &mut SPI_WRITE_BUF);
-    // syscall_spi_device.set_client(spi_syscalls);
 
     // LEDs
     let led_pins = static_init!(
@@ -415,37 +212,6 @@ pub unsafe fn reset_handler() {
     //     btn.set_client(button);
     // }
 
-    // // Setup ADC
-    // let adc_channels = static_init!(
-    //     [&'static sam4l::adc::AdcChannel; 6],
-    //     [
-    //         &sam4l::adc::CHANNEL_AD0, // A0
-    //         &sam4l::adc::CHANNEL_AD1, // A1
-    //         &sam4l::adc::CHANNEL_AD3, // A2
-    //         &sam4l::adc::CHANNEL_AD4, // A3
-    //         &sam4l::adc::CHANNEL_AD5, // A4
-    //         &sam4l::adc::CHANNEL_AD6, // A5
-    //     ]
-    // );
-    // let adc = static_init!(
-    //     capsules::adc::Adc<'static, sam4l::adc::Adc>,
-    //     capsules::adc::Adc::new(
-    //         &mut sam4l::adc::ADC0,
-    //         adc_channels,
-    //         &mut capsules::adc::ADC_BUFFER1,
-    //         &mut capsules::adc::ADC_BUFFER2,
-    //         &mut capsules::adc::ADC_BUFFER3
-    //     )
-    // );
-    // sam4l::adc::ADC0.set_client(adc);
-
-    // // Setup RNG
-    // let rng = static_init!(
-    //     capsules::rng::SimpleRng<'static, sam4l::trng::Trng>,
-    //     capsules::rng::SimpleRng::new(&sam4l::trng::TRNG, board_kernel.create_grant())
-    // );
-    // sam4l::trng::TRNG.set_client(rng);
-
     // set GPIO driver controlling remaining GPIO pins
     let gpio_pins = static_init!(
         [&'static e310x::gpio::GpioPin; 3],
@@ -464,35 +230,14 @@ pub unsafe fn reset_handler() {
     }
 
 
-
-    // hil::gpio::Pin::make_output(&e310x::gpio::PORT[22]);
-    // hil::gpio::Pin::clear(&e310x::gpio::PORT[22]);
-
-
-
     hil::gpio::Pin::make_output(&e310x::gpio::PORT[22]);
     hil::gpio::Pin::set(&e310x::gpio::PORT[22]);
 
     hil::gpio::Pin::make_output(&e310x::gpio::PORT[19]);
     hil::gpio::Pin::set(&e310x::gpio::PORT[19]);
 
-
     hil::gpio::Pin::make_output(&e310x::gpio::PORT[21]);
     hil::gpio::Pin::clear(&e310x::gpio::PORT[21]);
-
-
-    // // CRC
-    // let crc = static_init!(
-    //     capsules::crc::Crc<'static, sam4l::crccu::Crccu<'static>>,
-    //     capsules::crc::Crc::new(&mut sam4l::crccu::CRCCU, board_kernel.create_grant())
-    // );
-    // sam4l::crccu::CRCCU.set_client(crc);
-
-    // // DAC
-    // let dac = static_init!(
-    //     capsules::dac::Dac<'static>,
-    //     capsules::dac::Dac::new(&mut sam4l::dac::DAC)
-    // );
 
     let hifive1 = HiFive1 {
         // console: console,
@@ -503,14 +248,9 @@ pub unsafe fn reset_handler() {
         // humidity: humidity,
         // ninedof: ninedof,
         // spi: spi_syscalls,
-        // nrf51822: nrf_serialization,
-        // adc: adc,
         led: led,
         // button: button,
-        // rng: rng,
         // ipc: kernel::ipc::IPC::new(board_kernel),
-        // crc: crc,
-        // dac: dac,
     };
 
     // hail.console.initialize();
@@ -534,19 +274,12 @@ pub unsafe fn reset_handler() {
     );
     kernel::debug::set_debug_writer_wrapper(debug_wrapper);
 
-    // // Reset the nRF and setup the UART bus.
-    // hail.nrf51822.reset();
-    // hail.nrf51822.initialize();
-
-    // // Uncomment to measure overheads for TakeCell and MapCell:
-    // // test_take_map_cell::test_take_map_cell();
 
 
     e310x::uart::UART0.initialize_gpio_pins(&e310x::gpio::PORT[17], &e310x::gpio::PORT[16]);
 
 
     debug!("Initialization complete. Entering main loop");
-    // debug!("hibrad");
 
     extern "C" {
         /// Beginning of the ROM region containing app images.
